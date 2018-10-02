e80c7666b0af0b99ed63ec2210b688e7b59b8e4f
TanWaiKim <863550179@qq.com>
2018-10-02 23:20:54 +0800

    一切就绪，只欠东风。

--- src/main/java/cn/dgut/o2o/web/superadmin/AreaController.java
+++ src/main/java/cn/dgut/o2o/web/superadmin/AreaController.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,12 +25,16 @@ import cn.dgut.o2o.service.AreaService;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
 	@Autowired
 	private AreaService areaService;
 	
 	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea(){
+		logger.info("start...");
+		long startTime = System.currentTimeMillis();
+		
 		// 存放方法的返回值
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		// 存放service层的返回值
@@ -41,6 +48,10 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test.error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("end...");
 		return modelMap;
 	}
 	
